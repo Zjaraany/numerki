79797d4d34c71924af035ece7fe7d90496df9f93
247752 Oarek Magdalena <247752@edu.p.lodz.pl>
2024-03-02 16:38:10 +0100

    Dodane Falsi

---
+++ src/main/java/edu/num/Falsi.java
@@ -0,0 +1,57 @@
+package edu.num;
+
+public class Falsi implements Algorithm {
+    public Double algorithm(Double a, Double b, Integer iter, int functionChoice) {
+        int i = 1;
+
+        MathFunction mathFunction = Main.choice(functionChoice);
+
+        Double chordResult = a - (mathFunction.calculate(a) * (b - a)/(mathFunction.calculate(b)- mathFunction.calculate(a)));
+
+        Double value = mathFunction.calculate(chordResult);
+
+        if (value == 0) {
+            return chordResult;
+        } else {
+            while (i <= iter) {
+                value = mathFunction.calculate(chordResult);
+                Double valueA = mathFunction.calculate(a);
+                if (valueA * value < 0) {
+                    b = chordResult;
+                } else {
+                    a = chordResult;
+                }
+                chordResult = a - (mathFunction.calculate(a) * (b - a)/(mathFunction.calculate(b)- mathFunction.calculate(a)));
+
+                i++;
+            }
+            return chordResult;
+        }
+    }
+
+    public Double algorithm(Double a, Double b, Double eps, int functionChoice) {
+        MathFunction mathFunction = Main.choice(functionChoice);
+
+        Double chordResult = a - (mathFunction.calculate(a) * (b - a)/(mathFunction.calculate(b)- mathFunction.calculate(a)));
+
+        Double previous = Double.POSITIVE_INFINITY;
+
+        Double value = mathFunction.calculate(chordResult);
+        if (value == 0) {
+            return chordResult;
+        } else {
+            while (Math.abs(chordResult - previous) >= eps) {
+                previous = chordResult;
+                value = mathFunction.calculate(chordResult);
+                Double valueA = mathFunction.calculate(a);
+                if (valueA * value < 0) {
+                    b = chordResult;
+                } else {
+                    a = chordResult;
+                }
+                chordResult = a - (mathFunction.calculate(a) * (b - a)/(mathFunction.calculate(b)- mathFunction.calculate(a)));
+            }
+            return chordResult;
+        }
+    }
+}
